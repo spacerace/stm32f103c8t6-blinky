fbee5d5d3d96245b42015a96037b5a99a07b42d9
Nils Stec <stecdose@gmail.com>
2017-08-09 17:38:30 +0200

    moved from stm32loader.py to st-flash

--- write.sh
+++ write.sh
@@ -1,1 +1,5 @@
-stm32loader.py -e -w -p /dev/ttyUSB0 -b 115200 -g 0x08000000 output/main.bin
+# since stm32loader.py is a 'bit' outdated, i moved to st-util's st-flash.
+
+#stm32loader.py -e -w -p /dev/ttyUSB0 -b 115200 -g 0x08000000 output/main.bin
+st-flash write output/main.bin 0x8000000
+
